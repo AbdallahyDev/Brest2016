ba562942c00beb147f4536d747a348333c3eaeb0
Abdellahi <ouldethmane.abdellahi@gmail.com>
2016-02-13 11:42:49 +0100

    initialisation du projet

---
+++ src/bean/Visitor.java
@@ -0,0 +1,58 @@
+package bean;
+
+
+public class Visitor {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String psw;
+	private String phone;
+	private String mail;
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+}
